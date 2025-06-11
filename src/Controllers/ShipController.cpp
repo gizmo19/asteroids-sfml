77dd1fbec38367c271210cdc04f064fbe2e400b5
Dawid Nachlik <dnachlik@nocadis.com>
2025-06-11 07:26:49 +0200

    strzelanie na spacji

--- src/Controllers/ShipController.cpp
+++ src/Controllers/ShipController.cpp
@@ -63,7 +63,8 @@ void ShipController::updateMovement(float deltaTime) {
 }
 
 void ShipController::handleShooting() {
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+    bool shootPressed = sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Space);
+    if (shootPressed) {
         if (canShoot) {
             Message msg;
             msg.type = MessageType::BulletFired;
